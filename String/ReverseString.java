ef131c47dba9babeca19dc036e5b8c2e5fcb2579
sanket.mal@inworkglobal.com <sanket.mal@inworkglobal.com>
2026-06-19 20:17:54 +0530

    Reverse String Program

---
+++ String/ReverseString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        String reversed = reverseString(input);
+        System.out.println("Original: " + input);
+        System.out.println("Reversed: " + reversed);
+    }
+    public static String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+    public static String reverseStringUsingLoop(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+        return reversed;
+    }
+}
